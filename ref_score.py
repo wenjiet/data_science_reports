4315d71c728ea34a39a3b364778283bd5dbe3396
Salkexy <alex.yinan.wong@gmail.com>
2016-12-11 22:11:13 -0500

    report + code

---
+++ ref_score.py
@@ -0,0 +1,78 @@
+import config
+import pickle
+from collections import Counter
+import os
+import pandas as pd
+from Queue import Queue
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+def build_ref_dict():
+    print 'building'
+    ref_df = pd.read_csv(os.path.join(config.base_csv_dir, 'refs.csv'))
+    ref_dict = dict()
+    for _, row in ref_df.iterrows():
+        pid = int(row['paper_id'])
+        if pid not in ref_dict:
+            ref_dict[pid] = list()
+        ref_dict[pid].append(int(row['ref_id']))
+    save_obj(ref_dict, 'ref_idx')
+
+
+def load_ref_dict():
+    if not os.path.exists('ref_idx.pkl'):
+        build_ref_dict()
+
+    return load_obj('ref_idx')
+
+
+def graph_count(ref_idx, paper_id, niter=3):
+    vertices = list()
+    queue = Queue()
+    queue.put(paper_id)
+    for i in range(niter):
+        n = queue.qsize()
+        for j in range(n):
+            pid = queue.get()
+            try:
+                citations = ref_idx[pid]
+                for cite in citations:
+                    queue.put(cite)
+                    vertices.append(cite)
+            except:
+                continue
+
+    retval = dict()
+    vertices_cnt = Counter(vertices)
+    for pid in vertices_cnt:
+        retval[pid] = vertices_cnt[pid]
+
+    # normalization
+    max_val = max(retval.values())
+    for w in retval:
+        retval[w] = float(retval[w]) / max_val
+    return retval
+
+
+def test():
+    ref_dict = load_ref_dict()
+    print 'index loading done'
+    # paper_df = pd.read_csv(os.path.join(config.base_csv_dir, 'paper.csv'))
+    # title_dict = dict()
+    # for index, row in paper_df.iterrows():
+    #     title_dict[int(row['id'])] = row['title']
+    # print 'paper loading done'
+
+    pid = 93258
+    cnt = graph_count(ref_dict, pid, 3)
+    print cnt
+
+# test()
---
+++ title_similarity.py
@@ -0,0 +1,138 @@
+from nltk.corpus import stopwords
+import sys
+from collections import Counter
+# sys.path.insert(0, '../pipeline/')
+
+import config
+import os
+import pandas as pd
+import pickle
+
+
+def bigram_add(bigram_dict, key, value):
+    if key not in bigram_dict:
+        bigram_dict[key] = set()
+    bigram_dict[key].add(value)
+
+
+def bigram_index(titles):
+    """ Build the bigram index from the vocabulary words
+    Inputs:
+        words_dict(dict): The vocabulary words
+    Outputs:
+        dict: A python dictionary with the key of str, value of set of str
+    """
+    bigram = dict()
+
+    for key, title in titles.iteritems():
+        # filter stopwords
+        # print key, title
+        stop_words = set(stopwords.words('english'))
+        try:
+            words = [w.lower() for w in title.split(' ') if w not in stop_words]
+
+            if len(words) < 2:
+                bigram_add(bigram, words[0], key)
+                continue
+            for i in range(len(words) - 1):
+                bi = words[i: i + 2]
+                bigram_add(bigram, ' '.join(bi), key)
+        except:
+#            print 'exception:', key, title
+            continue
+    return bigram
+
+
+def bigram_search(bigram_dict, title):
+    """ Vague search the query word in the bigram index
+    Inputs:
+        bigram_dict(dict): The bigram index
+        word(str): The query word
+    Outputs:
+        list: A list of potential matching vocabulary
+    """
+    stop_words = set(stopwords.words('english'))
+    words = [w.lower() for w in title.split(' ') if w not in stop_words]
+
+    bi_list = list()
+    for i in range(len(words) - 1):
+        bi = ' '.join(words[i: i + 2])
+        if bi in bigram_dict:
+            bi_list += list(bigram_dict[bi])
+    bi_cnt = Counter(bi_list)
+    retval = dict()
+    for w in bi_cnt:
+        if bi_cnt[w] >= 2:
+            retval[w] = bi_cnt[w]
+
+    # normalization
+    max_val = max(retval.values())
+    for w in retval:
+        retval[w] = float(retval[w]) / max_val
+    return retval
+
+
+def build_bigram_index():
+    paper_df = pd.read_csv(os.path.join(config.base_csv_dir, 'paper.csv'))
+    title_dict = dict()
+    for index, row in paper_df.iterrows():
+        title_dict[int(row['id'])] = row['title']
+
+#    print type(title_dict)
+#    print title_dict.keys()[0], title_dict[title_dict.keys()[0]]
+
+    bigram_dict = bigram_index(title_dict)
+    save_obj(bigram_dict, 'bigram_idx')
+
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+def load_bigram_dict():
+    if not os.path.exists('./bigram_idx.pkl'):
+        build_bigram_index()
+    return load_obj('bigram_idx')
+
+
+def load_paper_dict():
+    if not os.path.exists('./paper_dict.pkl'):
+        paper_dict = dict()
+        paper_df = pd.read_csv(os.path.join(config.base_csv_dir, 'paper.csv'))
+        for _, row in paper_df.iterrows():
+            pid = int(row['id'])
+            paper_dict[pid] = dict()
+            paper_dict[pid]['title'] = row['title']
+            paper_dict[pid]['abstract'] = row['abstract']
+        save_obj(paper_dict, 'paper_dict')
+    return load_obj('paper_dict')
+
+def load_paper_inv_dict():
+    if not os.path.exists('./paper_inv_dict.pkl'):
+        paper_inv_dict = dict()
+        paper_df = pd.read_csv(os.path.join(config.base_csv_dir, 'paper.csv'))
+        for _, row in paper_df.iterrows():
+            pid = int(row['id'])
+            title = row['title']
+            paper_inv_dict[title] = pid
+        save_obj(paper_inv_dict, 'paper_inv_dict')
+    return load_obj('paper_inv_dict')
+
+
+def test():
+    bigram_dict = load_bigram_dict()
+    print 'bigram load done'
+    paper_dict = load_paper_dict()
+    print 'paper load done'
+    idx_list = bigram_search(bigram_dict, 'Novel approach on object recognition based on convolutional neural network')
+
+    for idx in idx_list:
+        # title_list.append(title_dict[idx])
+        print idx_list[idx], paper_dict[idx]
+
